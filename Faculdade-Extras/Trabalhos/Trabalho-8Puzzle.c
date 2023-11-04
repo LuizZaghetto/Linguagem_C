9ab1f74ff9bf2f842b9cb43add44fa23068fc563
Luiz Gustavo <luizgustavocampos2005@gmail.com>
2023-11-04 10:44:12 -0300

    Adicionando trabalho 04/11

--- Faculdade-Extras/Trabalhos/Trabalho-8Puzzle.c
+++ Faculdade-Extras/Trabalhos/Trabalho-8Puzzle.c
@@ -1,23 +1,5 @@
 #include "funcoes-8Puzzle.h"
 int main() {
-<<<<<<< HEAD
     jogar();
 }
-=======
 
-    int **matrizona;
-    matrizona = criaMatriz();
-    matrizona = prencheMatriz();
-    printaMatriz(matrizona);
-
-
-    //Libera o espaço ocupado pela matriz na memória
-    for (int i = 0; i < 3; i++) {
-    free(matrizona[i]);
-    }
-    free(matrizona);
-    
-    
-    return 0;
-}
->>>>>>> c820a1edb907ae6e2a543d521a3bda7508fc7069
